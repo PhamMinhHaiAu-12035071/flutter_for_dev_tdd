1792887f60bb6673a7f2d6f754fa0f2095b4daf5
pham minh hai au <aupmh@vitalify.asia>
2023-02-17 11:35:02 +0700

    ensure HttpAdapter calls post with correct headers.

--- test/data/infra/http/http_adapter_test.dart
+++ test/data/infra/http/http_adapter_test.dart
@@ -10,7 +10,11 @@ class HttpAdapter {
 
   Future<void> request(
       {required String url, required String method, Map? body}) async {
-    await client.post(Uri.parse(url));
+    final Map<String, String> headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(Uri.parse(url), headers: headers);
   }
 }
 
@@ -21,11 +25,16 @@ void main() {
     test('Should call post with correct values', () async {
       final url = faker.internet.httpUrl();
       final uri = Uri.parse(url);
+      final Map<String, String> headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      };
       final client = ClientSpy();
-      when(() => client.post(uri)).thenAnswer((_) async => Response('', 200));
+      when(() => client.post(uri, headers: headers))
+          .thenAnswer((_) async => Response('', 200));
       final sut = HttpAdapter(client: client);
       await sut.request(url: url, method: 'post');
-      verify(() => client.post(uri));
+      verify(() => client.post(uri, headers: headers));
     });
   });
 }
