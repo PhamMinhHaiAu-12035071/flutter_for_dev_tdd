44a5bf413f2d14a1e70157e73e26c2638920e319
pham minh hai au <aupmh@vitalify.asia>
2023-03-01 14:39:36 +0700

    ensure EmailValidation should return error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -9,10 +9,9 @@ class EmailValidation implements FieldValidation {
 
   @override
   String? validate(String? value) {
-    if (value != null && value.isEmpty || value == null) {
-      return null;
-    }
-    return null;
+    final regex = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
+    return isValid ? null : 'Email invalid';
   }
 }
 
@@ -29,5 +28,13 @@ void main() {
   test('Should return null if email is null', () {
     final error = sut.validate(null);
     expect(error, null);
+  });
+  test('Should return null if email is valid', () {
+    final error = sut.validate('phamau1994x@gmail.com');
+    expect(error, null);
+  });
+  test('Should return error if email is invalid', () {
+    final error = sut.validate('phamau1994x');
+    expect(error, 'Email invalid');
   });
 }
