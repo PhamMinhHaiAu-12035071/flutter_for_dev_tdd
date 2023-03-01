e333f181ead03c6d4d7f2d16f09a2543bdfd348b
pham minh hai au <aupmh@vitalify.asia>
2023-03-01 14:40:45 +0700

    move EmailValidation to its own file

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,19 +1,6 @@
 import 'package:flutter_for_dev_tdd/ui/validation/protocols/protocols.dart';
+import 'package:flutter_for_dev_tdd/ui/validation/validators/validators.dart';
 import 'package:test/test.dart';
-
-class EmailValidation implements FieldValidation {
-  @override
-  final String field;
-
-  EmailValidation(this.field);
-
-  @override
-  String? validate(String? value) {
-    final regex = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
-    return isValid ? null : 'Email invalid';
-  }
-}
 
 void main() {
   late FieldValidation sut;
