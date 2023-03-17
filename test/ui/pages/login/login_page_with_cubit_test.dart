75c77cd0c183f383bcac44b10d33e83e8162007f
pham minh hai au <aupmh@vitalify.asia>
2023-03-17 21:18:39 +0700

    Shoud load with correct initial state when use CubitLoginPresenter

---
+++ test/ui/pages/login/login_page_with_cubit_test.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_for_dev_tdd/domain/usecases/usecases.dart';
+import 'package:flutter_for_dev_tdd/presentation/presenters/presenters.dart';
+import 'package:flutter_for_dev_tdd/presentation/protocols/protocols.dart';
+import 'package:flutter_for_dev_tdd/ui/pages/pages.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ValidationSpy extends Mock implements Validation {}
+
+class AuthenticationSpy extends Mock implements Authentication {}
+
+class SaveCurrentAccountSpy extends Mock implements SaveCurrentAccount {}
+
+void main() {
+  late Validation validation;
+  late Authentication authentication;
+  late SaveCurrentAccount saveCurrentAccount;
+  late LoginPresenter presenter;
+
+  setUp(() {
+    validation = ValidationSpy();
+    authentication = AuthenticationSpy();
+    saveCurrentAccount = SaveCurrentAccountSpy();
+  });
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = CubitLoginPresenter(
+        validation: validation,
+        authentication: authentication,
+        saveCurrentAccount: saveCurrentAccount);
+    final loginPage = GetMaterialApp(
+      initialRoute: '/login',
+      getPages: [
+        GetPage(name: '/login', page: () => LoginPage(presenter: presenter)),
+        GetPage(
+            name: '/any_route',
+            page: () => const Scaffold(body: Text('fake page'))),
+      ],
+    );
+    await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets('Should load with correct initial state',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    /// Find the email TextFormField
+    final emailTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Email'),
+      matching: find.byType(Text),
+    );
+
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'When a TextFormField has only one text child, means it has no errors, since one of the children is always the label text',
+    );
+
+    /// Find the password TextFormField
+    final passwordTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Password'),
+      matching: find.byType(Text),
+    );
+
+    expect(passwordTextChildren, findsOneWidget,
+        reason:
+            'When a TextFormField has only one text child, means it has no errors, since one of the children is always the label text');
+
+    /// Find the login button
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+  });
+}
